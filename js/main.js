00f482514327147367508d63f36ca8d7d8ec7711
karo-801chan <karolina801chan@gmail.com>
2015-06-30 18:45:30 -0600

    v2 Modifique proyecto App style

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
-var showProject = {};
 
-showProject.administrador = function(data) {
+var showProject = (function () {
+
+  var closeButton = document.getElementById("close");
+  var contentInfo = document.getElementById('project-info');
+  var buttonNext;
+  var buttonPrevious;
+  var imgContainer;
+  var form = document.getElementById("form-contact")
+  var nameInput = document.getElementById("name");
+  var emailInput = document.getElementById("email");
+  var commentText = document.getElementById("comment");
 
   //Funcion para obtener la posicion de un elemento
   function elementPosition(idi) {
@@ -15,17 +24,6 @@ showProject.administrador = function(data) {
     };
   }
 
-  var TopLeft = elementPosition("project-info"); //"project-info" = div que quiero saber la posicion
-  console.log(TopLeft.top);
-  /*Muestra animacion cuando el scroll de la pagina esta en sobre mi
-  window.addEventListener("scroll", function(event) {
-    var left = this.scrollY;
-    if (left > TopLeft.top) {
-      document.getElementById("skills-bars").classList.remove('hidden-animation');
-    }
-  });
-  */
-  //Augmenting
   Function.prototype.method = function(name, func) {
     if (!this.prototype[name]) {
       this.prototype[name] = func;
@@ -43,8 +41,7 @@ showProject.administrador = function(data) {
     request.onload = function() {
       if (request.status >= 200 && request.status < 400) {
         var data = JSON.parse(request.responseText);
-        //console.log(data.project[0].id)
-        showInfo(data)
+        showInfo(data);
       } else {
          console.log("We reached our target server, but it returned an error")
       }
@@ -56,69 +53,64 @@ showProject.administrador = function(data) {
   }
 
   function showInfo(data) {
-    //busca todos los div con class = project-info
     var currentItem = document.querySelectorAll('.project-name');
     for (var i = 0; i < currentItem.length; i++) {
-      //muestra la informacion con click;
       currentItem[i].onclick = function() {
-        console.log(currentItem[i])
-        //hace scroll a la section de project-info;
-        scrollWin(231, 1580);
-        document.getElementById("close").classList.add('display-button');
+        closeButton.classList.add('display-button');
         var id = this.id;
         var contenido = "";
-        contenido +=
-        '<h2>' + data.project[id].name + ' /  <span>' + data.project[id].type + '</span></h2>' +
-        '<div class="info-project"><p>' + data.project[id].info + '</p></div>'+
-        '<div class="slider-project"><div class="web-vector"><div class="hideScrollBar"><img id="slider" src="' + data.project[id].img1 + '"></div></div><div class="slider-button"><button id="button-previous"><svg width="16px" height="25px"><path style="fill:#856D6D;" d="M0,11.253L15.469,0v3.25L2.434,12.477v0.095l13.036,9.179V25L0,13.794V11.253z"/></svg></button><button id="button-next"><svg width="16px" height="25px"><path style="fill:#856D6D;" d="M0,11.253L15.469,0v3.25L2.434,12.477v0.095l13.036,9.179V25L0,13.794V11.253z"/></svg></button></div></div>';
-        var contentInfo = document.getElementById('project-info');
+        contenido += '<h2>' + data.project[id].name + ' /  <span>' + data.project[id].type + '</span></h2> <p>' + data.project[id].info + '</p>';
+        if (data.project[id].type == "App") {
+          contenido +='<div class="slider-project"><div class="app-vector"><img id="slider" src="#"></div><div class="app-vector"><img id="slider" src="#"></div><div class="slider-button"><button id="button-previous"><svg width="16px" height="25px"><path style="fill:#856D6D;" d="M0,11.253L15.469,0v3.25L2.434,12.477v0.095l13.036,9.179V25L0,13.794V11.253z"/></svg></button><button id="button-next"><svg width="16px" height="25px"><path style="fill:#856D6D;" d="M0,11.253L15.469,0v3.25L2.434,12.477v0.095l13.036,9.179V25L0,13.794V11.253z"/></svg></button></div></div>';
+        }else {
+          contenido +='<div class="slider-project"><div class="web-vector"><div class="hideScrollBar"><img id="slider" src="' + data.project[id].img1 + '"></div></div><div class="slider-button"><button id="button-previous"><svg width="16px" height="25px"><path style="fill:#856D6D;" d="M0,11.253L15.469,0v3.25L2.434,12.477v0.095l13.036,9.179V25L0,13.794V11.253z"/></svg></button><button id="button-next"><svg width="16px" height="25px"><path style="fill:#856D6D;" d="M0,11.253L15.469,0v3.25L2.434,12.477v0.095l13.036,9.179V25L0,13.794V11.253z"/></svg></button></div></div>';
+        }
         contentInfo.innerHTML = contenido;
-        //cerrar la section proyect info;
-        document.getElementById("close").onclick = function() {
+        var infoPosition = elementPosition("close"); 
+        scrollWin(231, infoPosition.top);
+        closeButton.onclick = function() {
           contentInfo.innerHTML = "";
-          document.getElementById("close").classList.remove('display-button');
-          //hace scroll hacia arriba a la cuadricula de projectos;
-          scrollWin(231, 700)
+          closeButton.classList.remove('display-button');
+          var projectPosition = elementPosition("projects"); 
+          scrollWin(231, projectPosition.top);
         }
-        //Gallery json array
-        var imageGallery = [
-          data.project[id].img1,
-          data.project[id].img2,
-          data.project[id].img3
-        ];
-
-        var imgCount = 0;
-        var totalImgs = imageGallery.length - 1;
-        var buttonNext = document.getElementById("button-next");
-        var buttonPrevious = document.getElementById("button-previous");
-        //Augment
-        var imgContainer = document.getElementById("slider");
-        Element.method('cambiarSrc', function(c) {
-          this.src = c;
-          return this;
-        });
-        //Botones del slider
-        buttonNext.onclick = function() {
-          imgCount++;
-          if (imgCount > totalImgs) imgCount = 0
-          imgContainer.cambiarSrc(imageGallery[imgCount]);
-        }
-        buttonPrevious.onclick = function() {
-          imgCount--;
-          if (imgCount < 0) imgCount = totalImgs;
-          imgContainer.cambiarSrc(imageGallery[imgCount]);
-        }
+        sliderButton(data, id);
       }
     }
   }
-  loadJSON();
-  // Validacion del Form de Contacto.
-  var form = document.getElementById("form-contact")
-  var nameInput = document.getElementById("name");
-  var emailInput = document.getElementById("email");
-  var commentText = document.getElementById("comment");
 
-  document.getElementById("submit-button").onclick = function() {
+  function sliderButton(data, id){
+    //Gallery json array
+    var imageGallery = [
+      data.project[id].img1,
+      data.project[id].img2,
+      data.project[id].img3
+    ];
+
+    var imgCount = 0;
+    var totalImgs = imageGallery.length - 1;
+    buttonNext = document.getElementById("button-next");
+    buttonPrevious = document.getElementById("button-previous");
+    imgContainer = document.getElementById("slider");
+    //Augment
+    Element.method('cambiarSrc', function(c) {
+      this.src = c;
+      return this;
+    });
+    //Botones del slider
+    buttonNext.onclick = function() {
+      imgCount++;
+      if (imgCount > totalImgs) imgCount = 0
+      imgContainer.cambiarSrc(imageGallery[imgCount]);
+    }
+    buttonPrevious.onclick = function() {
+      imgCount--;
+      if (imgCount < 0) imgCount = totalImgs;
+      imgContainer.cambiarSrc(imageGallery[imgCount]);
+    }
+  }
+
+  function validationForm(){
     var chars = /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/;
     var regEx = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
     array_values = [];
@@ -133,33 +125,47 @@ showProject.administrador = function(data) {
       return this;
     });
 
-    if ( nameInput.value  === ""  || /^\s+$/.test(nameInput.value) || typeof nameInput.value !== 'string' ) {
+    if ( nameInput.value  === ""  || /^\s+$/.test(nameInput.value) || typeof nameInput.value != 'string' ) {
       nameInput.errorInput("Nombre Invalido").errorColor("red")
       return false;
-    } else {
-      array_values.push(nameInput.value);
     }
-
-    if (emailInput.value === "" || typeof emailInput.value !== "string" || !(emailInput).match(regEx) ) {
+    if (emailInput.value === "" || typeof emailInput.value != "string"  ) {
       emailInput.errorInput("Email Invalido").errorColor("red");
       return false;
-    }else {
-      array_values.push(emailInput);
     }
-
     if (commentText.value === "" || /^\s+$/.test(commentText.value) ) {
       commentText.errorInput("Deja un mensaje").errorColor("red");
       return false;
     } else {
-      array_values.push(commentText);
-      alert('El mensage fue enviado exitosamente, Gracias!');
       form.reset();
-      console.log("Contenido del formulario listo para enviar ", array_values);
       return false;
     }
   }
-}();
 
+  
+  //Muestra animacion cuando el scroll de la pagina esta en sobre mi
+  /*
+  window.addEventListener("scroll", function(event) {
+    var left = this.scrollY;
+    if (left > TopLeft.top) {
+      document.getElementById("skills-bars").classList.remove('hidden-animation');
+    }
+  });
+  */
+
+  return {
+    loadProjects : loadJSON,
+    validationForm : validationForm
+
+  };
+
+})();
+
+showProject.loadProjects();
+
+document.getElementById("submit-button").onclick = function() {
+  showProject.validationForm();
+}
 // Clean form.
 function message(input) {
   if(input.value == "Nombre Invalido" || input.value == "Email Invalido" || input.value == "Deja un mensaje") {
