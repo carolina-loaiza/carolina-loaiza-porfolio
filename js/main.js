eda685fcb93d8e770833c59f2d2d5a90fd615b16
karo-801chan <karolina801chan@gmail.com>
2015-12-11 16:43:28 -0600

    Agrego las funciones de la galeria y cambio los datos de los proyectos

--- js/main.js
+++ js/main.js
@@ -23,15 +23,19 @@
     var templateCopy ='<h2>{{name}}</h2><span>{{type}}</span>'+
                       '<p>{{copy}}</p>'+
                       '<a href={{git}} target="_blank" class="link">Github Code</a>'+
-                      '{{#link}}<a href={{link}} target="_blank" class="link">Web Site</a>{{/link}}';
-    var templateImage ='<div class="images-container">{{#image}}<img src={{src}}>{{/image}}</div>';
+                      '{{#link}}<a href={{link}} target="_blank" class="link">Web Site</a>{{/link}}'+
+                      '<div class="images-container">'+
+                        '<span>{{gallery}}</span>'+
+                        '<ul>{{#image}}<li id={{idx}}><img src={{src}}>{{/image}}</li></ul>'+
+                      '</div>';
+    var templateImage ='<picture>'+
+                        '<source media="(min-width: 480px)" srcset={{mainImage}}>'+
+                        '<source media="(max-width: 480px)" srcset={{mobileImage}}>'+
+                        '<img srcset={{mainImage}}>'+
+                      '</picture>';
 
     // Template Mustache Print Proyect
     $('.projects-container li').click(function(){
-      $('.project-section').removeClass("hidden");
-
-      // Scroll nav to Proyects
-      scrollAnimate(".project-section", 2000);
 
       var project = $(this).attr("id")
       // Mustache to html                
@@ -41,13 +45,26 @@
         var img = Mustache.to_html(templateImage, data);
         $('#project-copy').html(copy);
         $('#project-images').html(img);
+        $('.images-container').find('li:first-child').addClass('images-item');
+        $('.project-section').removeClass("hidden");
+        // Scroll nav to Proyects
+        scrollAnimate(".project-section", 2000);
       });
+    });
+
+     // Project Section Galery
+    $('.images-container li').click(function(){
+        var picture = $(this).children().attr('src');
+        var count = $(this).id;
+        var path = picture.substring(0, picturePath.indexOf('-')) + '-' + count + '.png';
+        console.log(path);
+        $('#project-images').find('img').src(path);
     });
 
     // Project Section Close Button
     $('.close-project-section').click(function(){
       scrollAnimate("#projects", 800);
-      $('.project-section').toggleClass("hidden");
+      $('.project-section').toggleClass('hidden');
     });
   });
 }());
