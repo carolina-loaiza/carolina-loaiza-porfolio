023ffede6d27b14295dda335ebc671518edc4f88
karo-801chan <karolina801chan@gmail.com>
2015-12-21 19:10:19 -0600

    Agrego error en datos.js

--- js/datos.js
+++ js/datos.js
@@ -3,7 +3,7 @@ var idx = 1;
 var projectOurHeritage = {
     name: "Our Heritage",
     type: "Sitio Web",
-    copy: "Our Heritage es un videojuego sobre el cambió climático que se desarrollo en conjunto con varias empresas y organizaciones incluyendo al CETAV. Para promocionar el videojuego se creo un equipo para desarrollar el sitio web. Nosotros utilizamos la metodologia Scrum para desarrollar el proyecto y cumplimos con varias etapas de elaboración de wireframes, guías de estilo, diseño y demos.",
+    copy: "Our Heritage es un videojuego sobre el cambio climático que se desarrolló en conjunto con varias empresas y organizaciones incluyendo al CETAV. Para promocionar el videojuego se creó un equipo para desarrollar el sitio web. Nosotros utilizamos la metodología Scrum para desarrollar el proyecto y cumplimos con varias etapas de elaboración de wireframes, guías de estilo, diseño y demos.",
     git: "https://github.com/our-heritage/our-heritage.github.io",
     link: "http://ourheritagegame.com",
     mainImage: "img/project-ourHeritage/ourHeritage-01.png",
@@ -39,7 +39,7 @@ var projectGameJam = {
 var projectExtremeKawaii = {
     name: "Extreme Kawaii",
     type: "Drupal 7",
-    copy: "Extreme Kawaii fue un proyecto que inició como un sitio estático, construido con HTML y CSS. A pesar de que el sitio no es 100% funcional, la idea principal del proyecto fue lograr pasar el sitio estático a un CMS como Drupal 7, para esto se trabajo en parejas y utilizamos herramientas con pantheon.io , Github , Sass, y Vagrant para desarrollar en un ambiente local",
+    copy: "Extreme Kawaii fue un proyecto que inició como un sitio estático, construido con HTML y CSS. A pesar de que el sitio no es 100% funcional, la idea principal del proyecto fue lograr pasar el sitio estático a un CMS como Drupal 7, para esto se trabajo en parejas y utilizamos herramientas con pantheon.io , Github , Sass, y Vagrant para desarrollar en un ambiente local.",
     link: "http://dev-extreme-kawaii.pantheon.io/",
     mainImage: "img/project-extremeKawaii/extremeK-01.png",
     gallery: "Galería",
@@ -55,7 +55,7 @@ var projectExtremeKawaii = {
 var projectMusicPlayer = {
     name: "Music Player",
     type: "Web App",
-    copy: "La información, imágenes y audio del reproductor de música son obtenidas del API publico de Deezer por medio de un llamado ajax. La funcionalidad de los controles del reproductor es por medio de javascript, utilice Revealing Module , de forma que cada control del reproductor tiene su propia función, utilizando los elementos audio de HTML 5.",
+    copy: "La información, imágenes y audio del reproductor de música son obtenidas del API público de Deezer por medio de un llamado ajax. La funcionalidad de los controles del reproductor es por medio de javascript, utilice Revealing Module , de forma que cada control del reproductor tiene su propia función, utilizando los elementos audio de HTML5.",
     git: "https://github.com/carolina-loaiza/Music-Player",
     mainImage: "img/project-musicPlayer/musicPlayer-01.png"
 };
@@ -63,7 +63,7 @@ var projectMusicPlayer = {
 var projectWeatherApp = {
     name: "Weather App",
     type: "App",
-    copy: "La aplicación automáticamente muestra el pronóstico del tiempo de la ubicación actual y los siguientes tres días de la semana. También esta la opción de hacer una consulta por ciudad y país. En este proyecto utilice el API publico de OpenWeatherMap, para obtener toda la información necesaria con respecto al clima. La información es consultada por medio de un llamado ajax y se muestra al usuario por medio de javascript. Los iconos utilizados son una tipografía de uso libre.",
+    copy: "La aplicación automáticamente muestra el pronóstico del tiempo de la ubicación actual y los siguientes tres días de la semana. También está la opción de hacer una consulta por ciudad y país. En este proyecto utilice el API público de OpenWeatherMap, para obtener toda la información necesaria con respecto al clima. La información es consultada por medio de un llamado ajax y se muestra al usuario por medio de javascript. Los iconos utilizados son una tipografía de uso libre.",
     git: "https://github.com/carolina-loaiza/Weather-App",
     mainImage: "img/project-weatherApp/weatherApp-01.png"
 };
